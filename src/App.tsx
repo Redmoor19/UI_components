f476b839dc4e46e1fbc17179c192f997e47d9772
Redmoor <danilamandrabura@gmail.com>
2025-05-28 16:49:59 +0300

    feat: Implement basic expandable list in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,50 @@
+import { useState } from "react";
+import { Expandable } from "./Components/expandable/ui/Expandable";
+import { ExpandableContent } from "./Components/expandable/ui/ExpandableContent";
+import { ExpandableList } from "./Components/expandable/ui/ExpandableList";
+import { ExpandableTrigger } from "./Components/expandable/ui/ExpandableTrigger";
+
 function App() {
+  const [show, setShow] = useState(true);
+
 
   return (
-    <div className="h-2" >
-      Jopa
-    </div>
-  )
+    <ExpandableList>
+      <button onClick={() => setShow(prev => !prev)}>Show</button>
+      {show && <Expandable id="1">
+        <ExpandableTrigger>
+          <div>Trigger</div>
+        </ExpandableTrigger>
+        <ExpandableContent>
+          <div>Content</div>
+        </ExpandableContent>
+      </Expandable>}
+      <Expandable id="2">
+        <ExpandableTrigger>
+          <div>Trigger</div>
+        </ExpandableTrigger>
+        <ExpandableContent>
+          <div>Content</div>
+        </ExpandableContent>
+      </Expandable>
+      <Expandable id="3">
+        <ExpandableTrigger>
+          <div>Trigger</div>
+        </ExpandableTrigger>
+        <ExpandableContent>
+          <div>Content</div>
+        </ExpandableContent>
+      </Expandable>
+      <Expandable id="4">
+        <ExpandableTrigger>
+          <div>Trigger</div>
+        </ExpandableTrigger>
+        <ExpandableContent>
+          <div>Content</div>
+        </ExpandableContent>
+      </Expandable>
+    </ExpandableList>
+  );
 }
 
-export default App
+export default App;

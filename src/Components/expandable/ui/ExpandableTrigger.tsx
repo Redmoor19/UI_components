4b54b51a5c7a71aa4e73a18c057543fe5a1cb4e0
Redmoor <danilamandrabura@gmail.com>
2025-05-29 17:57:41 +0300

    refactor: Make ExpandableTrigger a button with full width

--- src/Components/expandable/ui/ExpandableTrigger.tsx
+++ src/Components/expandable/ui/ExpandableTrigger.tsx
@@ -1,16 +1,19 @@
-import { useExpandableContext } from "./Expandable";
+import { twMerge } from "tailwind-merge";
+import { useExpandableContext } from "../context/ExpandableContext";
 
 export type ExpandableTriggerProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export const ExpandableTrigger: React.FC<ExpandableTriggerProps> = ({
   children,
+  className
 }) => {
-  const { toggleCurrentExpanded } = useExpandableContext();
+  const { toggleCurrentExpanded, triggerRef } = useExpandableContext();
 
   return (
-    <button onClick={toggleCurrentExpanded} type="submit">
+    <button className={twMerge("block w-full", className)} ref={triggerRef} onClick={toggleCurrentExpanded} type="submit">
       {children}
     </button>
   );

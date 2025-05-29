7fc95bb26176b8d6bbb2bd3a388344bb71399221
Redmoor <danilamandrabura@gmail.com>
2025-05-29 17:58:16 +0300

    feat: Add useExpand hook and ExpandableContent component

--- src/Components/expandable/ui/ExpandableContent.tsx
+++ src/Components/expandable/ui/ExpandableContent.tsx
@@ -1,18 +1,28 @@
-import { useExpandableContext } from "./Expandable";
+import { twMerge } from "tailwind-merge";
+import { useExpandableContext } from "../context/ExpandableContext";
 
 type ExpandableContentProps = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export const ExpandableContent: React.FC<ExpandableContentProps> = ({
   children,
+  className
 }) => {
-  const { isCurrentExpanded, isInsideContext } = useExpandableContext();
+  const { isInsideContext, contentRef } =
+    useExpandableContext();
   if (!isInsideContext)
     throw new Error("'ExpandableContent' has to be inside 'Expandable'");
 
   return (
-    <div className={`${isCurrentExpanded ? "bg-green-500" : "bg-red-500"} h-5`}>
+    <div
+      ref={contentRef}
+      className={twMerge(
+        "overflow-hidden transition-all duration-300",
+        className
+      )}
+    >
       {children}
     </div>
   );

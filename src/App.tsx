b7030894d18b6a9695670483ee5bcfbeaff66a6d
Redmoor <danilamandrabura@gmail.com>
2025-05-31 13:42:57 +0300

    feat: Implement ExpandableList component with data mapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import { Expandable } from "./Components/expandable/ui/Expandable";
 import { ExpandableContent } from "./Components/expandable/ui/ExpandableContent";
+import { ExpandableList } from "./Components/expandable/ui/ExpandableList";
 import { ExpandableTrigger } from "./Components/expandable/ui/ExpandableTrigger";
+
+const items = [
+  {
+    title: "Introduction",
+    description: "The opening section that sets the stage.",
+  },
+  { title: "Methods", description: "A detailed look at the techniques used." },
+  { title: "Results", description: "The outcomes and findings of the study." },
+  {
+    title: "Discussion",
+    description: "An interpretation and analysis of the results.",
+  },
+  {
+    title: "Conclusion",
+    description: "A summary of the main points and implications.",
+  },
+];
 
 function App() {
   return (
-    <div className="h-dvh flex justify-center items-center">
-      <Expandable axle="horisontal">
-        <ExpandableContent
-          className="border border-red-400 p-2 w-96"
-        >
-          Aliquam erat volutpat. Nunc eleifend leo vitae magna. In id erat non
-          orci commodo lobortis. Proin neque massa, cursus ut, gravida ut,
-          lobortis eget, lacus. Sed diam. Praesent fermentum tempor tellus.
-          Nullam tempus. Mauris ac felis vel velit tristique imperdiet. Donec at
-          pede. Etiam vel neque nec dui dignissim bibendum. Vivamus id enim.
-          Phasellus neque orci, porta a, aliquet quis, semper a, massa.
-          Phasellus purus. Pellentesque tristique imperdiet tortor. Nam euismod
-          tellus id erat.
-        </ExpandableContent>
-        <ExpandableTrigger className="px-3">Trigger</ExpandableTrigger>
-      </Expandable>
+    <div className="h-dvh flex flex-col justify-center items-center">
+      <ExpandableList>
+        {items.map((item) => (
+          <Expandable key={item.title} id={item.title}>
+            <ExpandableTrigger>{item.title}</ExpandableTrigger>
+            <ExpandableContent>{item.description}</ExpandableContent>
+          </Expandable>
+        ))}
+      </ExpandableList>
     </div>
   );
 }

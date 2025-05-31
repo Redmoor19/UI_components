725fedc99abb3a2ac4b5d7b06678ff103dadc336
Redmoor <danilamandrabura@gmail.com>
2025-05-31 11:38:26 +0300

    feat: Add dynamic height/width to ExpandableContent

--- src/Components/expandable/ui/ExpandableContent.tsx
+++ src/Components/expandable/ui/ExpandableContent.tsx
@@ -1,29 +1,67 @@
+import { useEffect } from "react";
 import { twMerge } from "tailwind-merge";
 import { useExpandableContext } from "../context/ExpandableContext";
 
 type ExpandableContentProps = {
   children: React.ReactNode;
   className?: string;
+  wrapperClassName?: string;
+  isAbsolute?: boolean;
 };
 
 export const ExpandableContent: React.FC<ExpandableContentProps> = ({
   children,
-  className
+  className,
+  wrapperClassName,
+  isAbsolute,
 }) => {
-  const { isInsideContext, contentRef } =
+  const { isInsideContext, contentRef, triggerRef, isCurrentExpanded, axle } =
     useExpandableContext();
+
   if (!isInsideContext)
     throw new Error("'ExpandableContent' has to be inside 'Expandable'");
 
+  useEffect(() => {
+    const triggerEl = triggerRef?.current;
+    const contentEl = contentRef?.current;
+
+    if (!triggerEl || !contentEl) return;
+
+    const height = contentEl.scrollHeight;
+    const width = contentEl.scrollWidth;
+
+    const contentBefore =
+      contentEl.compareDocumentPosition(triggerEl) ===
+      Node.DOCUMENT_POSITION_FOLLOWING;
+
+    switch (axle) {
+      case "vertical":
+        contentEl.style.height = isCurrentExpanded ? `${height}px` : "0px";
+        if (contentBefore) contentEl.style.bottom = "100%";
+        contentEl.style.top = "100%";
+        break;
+      case "horisontal":
+        contentEl.style.width = isCurrentExpanded ? `${width}px` : "0px";
+        if (contentBefore) contentEl.style.left = "100%";
+        contentEl.style.right = "100%";
+        break;
+      default:
+        break;
+    }
+  }, [isCurrentExpanded, triggerRef.current, contentRef.current, axle]);
+
   return (
     <div
-      ref={contentRef}
       className={twMerge(
-        "overflow-hidden transition-all duration-300",
-        className
+        !isCurrentExpanded && axle === "vertical" && "h-0",
+        !isCurrentExpanded && axle === "horisontal" && "w-0",
+        "overflow-hidden transition-all duration-300 flex-1",
+        isAbsolute && "absolute",
+        wrapperClassName,
       )}
+      ref={contentRef}
     >
-      {children}
+      <div className={className}>{children}</div>
     </div>
   );
 };
